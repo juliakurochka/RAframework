28ff5bd458e276437a722b23622781d80af37975
timrsfo <timr_317@yahoo.com>
2014-03-15 14:43:36 -0700

    expectedException update

--- rideauction/src/test/java/com/sqa/ra/adapters/RACouponAdapterTest.java
+++ rideauction/src/test/java/com/sqa/ra/adapters/RACouponAdapterTest.java
@@ -17,6 +17,7 @@ public class RACouponAdapterTest {
 		couponAdapter=new RACouponAdapter();
 		
 	}
+	
   @Test(expectedExceptions = RuntimeException.class)
   public void addMoreCoupon() {
     throw new RuntimeException("Test not implemented");
