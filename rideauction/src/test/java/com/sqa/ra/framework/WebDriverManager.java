9d12cd5be643836945310e1068c375df7926b248
TrainerZiffowinfotech <zffow@192.168.1.8>
2014-03-15 15:29:10 -0700

    Making Changes for IE and Chrome

--- rideauction/src/test/java/com/sqa/ra/framework/WebDriverManager.java
+++ rideauction/src/test/java/com/sqa/ra/framework/WebDriverManager.java
@@ -1,37 +1,56 @@
 package com.sqa.ra.framework;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverManager {
-	
-	public static WebDriver getWebDriver(){
+
+	public static WebDriver getWebDriver() {
 		WebDriver driver;
-		String browser=PropertyManager.getProperty("browser");
-		if("firefox".equalsIgnoreCase(browser)){
-			driver=new FirefoxDriver();
+		String browser = PropertyManager.getProperty("browser");
+		if ("firefox".equalsIgnoreCase(browser)) {
+			DesiredCapabilities cap=DesiredCapabilities.firefox();
+			//cap.setVersion("3.7");
+			//cap.setPlatform(Platform.WINDOWS);
+			//cap.set
+			driver = new FirefoxDriver(cap);
+
+		} else if ("ie".equalsIgnoreCase(browser)) {
 			
-		}else if("ie".equalsIgnoreCase(browser)){
-			driver=new InternetExplorerDriver();
+			File file = new File(PropertyManager.getProperty("InternetExplorerServerPath"));
+			//get the path of webdriver exe
 			
-		} else if("chrome".equalsIgnoreCase(browser)){
-			driver=new ChromeDriver();
+			System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+
+			DesiredCapabilities capabilities = DesiredCapabilities. internetExplorer();
+			capabilities.setCapability(" ignoreZoomSetting", true);
+			capabilities.setCapability( InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			 driver =  new InternetExplorerDriver( capabilities);
+			driver = new InternetExplorerDriver();
+
+		} else if ("chrome".equalsIgnoreCase(browser)) {
 			
-		}else{
-			driver=new HtmlUnitDriver();
+			File file = new File(PropertyManager.getProperty("ChromeServerPath"));
+			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+			driver = new ChromeDriver();
+
+		} else {
+			driver = new HtmlUnitDriver();
 		}
-		
+
 		driver.get(PropertyManager.getProperty("appurl"));
-		driver.manage().timeouts().
-		implicitlyWait(10, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 		return driver;
-		
+
 	}
 
 }
