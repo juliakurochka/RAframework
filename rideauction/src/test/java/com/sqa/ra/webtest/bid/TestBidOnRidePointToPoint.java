de213b2220819f39fc7c4739199ffd75eb1a6959
juliakurochka <juliakurochka@gmail.com>
2014-03-23 21:03:59 -0700

    Added test class for Point to Point

---
+++ rideauction/src/test/java/com/sqa/ra/webtest/bid/TestBidOnRidePointToPoint.java
@@ -0,0 +1,20 @@
+package com.sqa.ra.webtest.bid;
+
+import org.testng.annotations.Test;
+
+import com.sqa.ra.framework.WebDriverTest;
+import com.sqa.ra.page.BidOnRidePointToPointPage;
+import com.sqa.ra.page.RAHomePage;
+
+public class TestBidOnRidePointToPoint extends WebDriverTest{
+
+	@Test
+	public TestBidOnRidePointToPoint(){
+		
+		
+		RAHomePage homePage = new RAHomePage(driver);
+		BidOnRidePointToPointPage ptpPage;
+		
+	}
+
+}
