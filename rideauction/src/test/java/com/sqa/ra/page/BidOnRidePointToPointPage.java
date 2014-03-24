08ceb82401f7e19e723b8e0a84af56a9eaf403ba
juliakurochka <juliakurochka@gmail.com>
2014-03-23 20:40:03 -0700

    Created new page

---
+++ rideauction/src/test/java/com/sqa/ra/page/BidOnRidePointToPointPage.java
@@ -0,0 +1,26 @@
+package com.sqa.ra.page;
+
+import org.openqa.selenium.WebDriver;
+
+import com.sqa.ra.framework.RAPage;
+
+public class BidOnRidePointToPointPage extends RAPage {
+
+	public BidOnRidePointToPointPage(WebDriver driver) {
+		super(driver);
+		
+	}
+
+	@Override
+	public void waitForPageToLoad() {
+		
+
+	}
+
+	@Override
+	public void verifyPageElements() {
+		
+
+	}
+
+}
